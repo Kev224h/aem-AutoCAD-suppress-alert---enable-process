5a6a8c1bd3ca5496826d8794f5f161f1cbd18307
Kev224h <63749541+Kev224h@users.noreply.github.com>
2020-10-30 12:50:39 +0000

    Add files via upload

---
+++ CreateRegistryKeyandPropertiesAcrobatCAD.ps1
@@ -0,0 +1,4 @@
+﻿#_CreateRegistryKeyandProperties
+New-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\2017" -ItemType Key -Name "FeatureState"
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\2017\FeatureState" -PropertyType DWORD -Name "bSuppressODADialog" -Value "1"
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\2017\FeatureState" -PropertyType DWORD -Name "bEnableODAWorkflow" -Value "1"
